0b78cff0d1902bd25177d4028b78621f6aef4014
Adam Zielinski <azielu@gmail.com>
2020-09-06 02:44:01 +0200

    added test for MainViewModel

--- app/src/main/java/com/azapps/alarmapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/azapps/alarmapp/ui/main/MainViewModel.kt
@@ -3,14 +3,20 @@ package com.azapps.alarmapp.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.azapps.alarmapp.data.db.entities.Alarm
 import com.azapps.alarmapp.data.repository.AlarmRepository
+import com.azapps.alarmapp.di.module.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(private val alarmRepository: AlarmRepository) :
+class MainViewModel @Inject constructor(
+    @IoDispatcher ioDispatcher: CoroutineDispatcher,
+    private val alarmRepository: AlarmRepository) :
     ViewModel() {
+    private val ioContext = viewModelScope.coroutineContext + ioDispatcher
 
-    val getAlarmRinging: LiveData<Alarm?> =
+    fun getAlarmRinging() =
         Transformations.map(alarmRepository.getAlarmRinging) { data -> data }
 
 }
